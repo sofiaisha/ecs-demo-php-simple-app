7a6e07970a73a5f60ecb499d9f776163a0fac0bc
Romero Corral, Sofia <sofiaro@amazon.com>
2017-12-14 14:46:42 -0600

    hi img

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$image = 'http://www.google.com/doodle4google/images/d4g_logo_global.jpg';
+$image = 'https://s3.amazonaws.com/sofy/2jD7ZHg.jpg';
 $imageData = base64_encode(file_get_contents($image));
 echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 ?>
