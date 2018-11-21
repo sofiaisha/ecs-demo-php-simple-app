1fb67cb2918293af4a490d5d3615b425981f1222
Sofy <pequeaisha@hotmail.com>
2018-11-20 22:02:03 -0600

    New Content

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Simple PHP app v5</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -15,10 +15,11 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Aplicacion de prueba</h1>
-                <h2>Felicidades</h2>
-                <p>Tu aplicacion PHP ahora esta corriendo en Amazon ECS.</p>
-                <p>El container esta corriendo la version de PHP <?php echo phpversion(); ?>.</p>
+                <h1>Simple PHP App v5</h1>
+                <h2>Gracias Reachcore</h2>
+                <h2>http://bit.ly/2JfOAo4</h2>                
+                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
--- src/index.php
+++ src/index.php
@@ -1,3 +1,39 @@
-<?php
-echo '<img src="https://s3.amazonaws.com/sofy/2jD7ZHg.jpg"/>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>PHP APP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <style>body {margin-top: 40px; background-color: #333;}</style>
+        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="hero-unit">
+                <h1>Aplicacion PHP con CI/CD</h1>
+                <h2>GRACIAS TOTALES</h2>  
+                <h2>Guadalajara</h2>                
+                <p>Tu aplicacion PHP esta siendo ejecutada usando CodePipeline, CodeBuild, y ECS-Fargate.</p>
+                <p>devops strategy!!</p>                
+                <br>
+                <br>
+                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <?php
+                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                        echo fread($myfile,filesize("/var/www/my-vol/date"));
+                        fclose($myfile);
+                ?>
+
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
